937613b9191feb84ea7ae9289552afb080d244f9
Yura Vishnyakov <yuravishnyakov@Yuras-MacBook-Air.local>
2020-08-27 23:28:24 +0300

    update canvas

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private List<Hook> _hooks;
+    [SerializeField] private GameObject _container;
+    [SerializeField] private HookView _template;
+    [SerializeField] private Player _player;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < _hooks.Count; i++)
+        {
+            AddHook(_hooks[i]);
+        }
+    }
+
+    private void AddHook(Hook hook)
+    {
+        var item = Instantiate(_template, _container.transform);
+        
+        item.Render(hook);
+    }
+}
